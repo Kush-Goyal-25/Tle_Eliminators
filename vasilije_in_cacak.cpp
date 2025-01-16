3f6270fcba1a2e39b4bb6d71765cd866713651a8
Kush Goyal <kush.goyal.personal@gmail.com>
2025-01-16 23:10:43 +0530

    Add files via upload

---
+++ vasilije_in_cacak.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <climits>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+
+  while (t--) {
+
+    ll n, k, x;
+    cin >> n >> k >> x;
+
+    ll low = (k * (k + 1)) / 2;
+    ll high = (n * (n + 1)) / 2 - ((n - k) * (n - k + 1)) / 2;
+
+    if (x >= low && x <= high) {
+      cout << "Yes" << endl;
+    } else {
+      cout << "No" << endl;
+    }
+  }
+}
+
+// n ==> 1 to n
+// k ==> k distinct int
+// x ==> sum
+//
