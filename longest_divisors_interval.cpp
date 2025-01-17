ed1303f9e8b9aec7577381359954a098178c10f4
Kush Goyal <kush.goyal.personal@gmail.com>
2025-01-17 22:33:30 +0530

    Add files via upload

---
+++ longest_divisors_interval.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <climits>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+
+  while (t--) {
+    ll n;
+    cin >> n;
+    ll i = 1;
+
+    while (n % i != 0) {
+      i++;
+    }
+    cout << i << endl;
+  }
+}
