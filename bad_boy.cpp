d78a3b2ab4f67c0e7c0be7c3aaee22f6eeb5fe39
Kush Goyal <kush.goyal.personal@gmail.com>
2025-02-02 22:00:24 +0530

    Add files via upload

---
+++ bad_boy.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <iostream>
+#include <vector>
+using namespace std;
+#define ll long long
+
+int main() {
+  int t;
+  cin >> t;
+
+  while (t--) {
+    long long n, m, i, j;
+    cin >> n >> m >> i >> j;
+
+    long long x1 = 1, y1 = 1;
+    long long x2 = n, y2 = m;
+
+    cout << x1 << " " << y1 << " " << x2 << " " << y2 << endl;
+  }
+  return 0;
+}
